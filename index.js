8cfcf355d1df49a0e75cace391b233bf9efb7015
FireStreaker2 <103970465+FireStreaker2@users.noreply.github.com>
2023-04-13 10:18:24 -0700

    rework to prevent exploit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
@@ -11,23 +10,56 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-var jsonParser = bodyParser.json();
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.get("/", async (req, res) => {
 
-app.use(express.static('static'));
-
-app.post("/endpoint", urlencodedParser, async function (req, res) {
-  
-  var query = req.body.query;
   const response = await openai.createImage({
-    prompt: query,
+    prompt: "Watermelon",
     n: 1,
     size: "256x256",
     response_format: "url",
-  });
+  })
 
-  image_url = response.data.data[0].url;
-  res.send(image_url);
+  var image_url = response.data.data[0].url;
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Watermelon">
+        <meta name="keywords" content="FireStreaker2, Watermelon" />
+        <meta name=”copyright” content=”FireStreaker2”>
+        <meta property="og:title" content="Watermelon" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://github.com/FireStreaker2/Watermelon" />
+        <meta property="og:image" content="https://firestreaker2.gq/watermelonFavicon.png" />
+        <meta property="og:description" content="Watermelon" />
+        <meta name="theme-color" content="#000000">
+        <meta name="twitter:card" content="summary_large_image">
+        
+        <title>Watermelon</title>
+        <link rel="icon" type="image/x-icon" href="https://firestreaker2.gq/watermelonFavicon.png" />
+      </head>
+
+      <body>
+        <style>
+          * {
+            margin: 0;
+          }
+
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        </style>
+        <img src="${image_url}" />
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port || 3000, () => {
